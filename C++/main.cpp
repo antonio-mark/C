9c426c7946afa824afb121827bccbdb3257c7a12
Antonio Borgo Neto <AntonioNeto3@edu.unisinos.br>
2023-08-07 21:53:32 -0300

    add aula 1

---
+++ C++/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    char c = 'A';
+    float f = 2.34;
+    cout << i << '\t' << c << '\t' << f << endl;
+    return 0;
+}
---
+++ C/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade = 21;
+    float altura = 1.80;
+
+    printf("Antonio\t");
+    printf("%d\t", idade);
+    printf("%.2f\t", altura);
+
+    printf("\n********");
+    printf("\n********");
+    printf("\n********");
+    printf("\n********");
+
+    printf("\n");
+    printf("\n********\n********\n********\n********");
+
+    return 0;
+}
